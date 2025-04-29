d4b299c3f5f5f6737e1776a138c0d10df89477a9
meganmason <meg.mason12@gmail.com>
2025-04-29 14:36:32 -0700

    updated start_log_rtd.sh file

--- scripts/start_log_rtd.sh
+++ scripts/start_log_rtd.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 sleep 60
 source /home/meganmason/rpi/bin/activate
-python /home/meganmason/Documents/projects/cold-content/snowtemps_raspi/scripts/log_rtd.py
+cd /home/meganmason/Documents/projects/cold-content/snowtemps_raspi/scripts
+/home/meganmason/rpi/bin/python log_rtd.py
